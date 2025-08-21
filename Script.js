6106ab15298493a4d1767cad2b9fa3d663a0693c
pavanvish46-tech <pavanvish46@gmail.com>
2025-08-21 13:28:29 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("checkBtn").addEventListener("click", async () => {
-  const res = await fetch("/api/check");
-  const data = await res.json();
-  document.getElementById("result").innerText = data.message;
-});
